af99b163e576f07598032fae0f64ce022df1800b
Arun Khanal <zarunkhanal1@gmail.com>
2025-02-15 14:49:24 +0545

    scratch

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-900 text-white p-4 text-center">
-      <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
